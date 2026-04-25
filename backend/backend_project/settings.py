89b743a008655c335b5553a48766cb5aec4f5065
tripti <triptikothari1999@gmail.com>
2026-04-25 13:52:59 +0530

    deploymengt changes added

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -24,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', "iooi")
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-local-dev-key-please-change-in-prod')
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
